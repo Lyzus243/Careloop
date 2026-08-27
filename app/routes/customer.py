7371ac1ec082b798b167df176e2506a9ae3984b1
Lyzus243 <olaniranj6@gmail.com>
2026-08-27 17:08:12 +0100

    customer edit

--- app/routes/customer.py
+++ app/routes/customer.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from fastapi import APIRouter, Depends, HTTPException, Query, status, Request
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 
 from app.database import get_db
 from app.dependencies import get_current_user_id
